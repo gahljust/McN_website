158601627c9a0d8a543d034d0beb66b9110d7f66
gahljust <gahljust@isu.edu>
2023-02-20 12:58:51 -0700

    First commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import Image from 'next/image'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+    <>
+      <Image src="/images/logo.svg" width={20} height={20} alt="LENS" />
+      <span>LENS</span>
+    </>
+  ),
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
@@ -11,7 +17,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'LENS 2023',
   },
 }
 
