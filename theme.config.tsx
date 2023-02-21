68b4973eb8a1fa83d4e46a732454c3b34e2b9b0c
gahljust <gahljust@isu.edu>
2023-02-21 09:57:51 -0700

    try again

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import Image from 'next/image'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  // Add the logo image to the logo config with the Name
+  logo: ( <>
+            <Image src="/images/logo.svg" alt="Logo" width={20} height={20} />
+    <span style={{ marginLeft: '.4em', fontWeight: 700 }}>LENS</span>
+          </>
+  ),
+
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
