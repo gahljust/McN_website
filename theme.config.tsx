1ef8bbe3f738e238c723eba2de49bd5b4f0475eb
gahljust <gahljust@isu.edu>
2023-04-27 11:16:48 -0600

    added and changed logos

--- theme.config.tsx
+++ theme.config.tsx
@@ -6,8 +6,8 @@ const config: DocsThemeConfig = {
   // Add the logo image to the logo config with the Name
   logo: (
     <>
-      <Image src="/images/logo.svg" alt="Logo" width={20} height={20} />
-      <span style={{ marginLeft: '.4em', fontWeight: 700 }}>LENS</span>
+      <Image src="/images/bengal_logo.png" alt="Logo" width={50} height={50} />
+      <span style={{ marginLeft: '.4em', fontWeight: 700 }}></span>
     </>
   ),
 
@@ -19,8 +19,32 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/gahljust/McN_website',
   footer: {
-    text: '2023 © Lens. All rights reserved.',
+    text:
+      <div style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+      }}>
+        <div>
+      ISU {new Date().getFullYear()} © <a href="https://www2.cose.isu.edu/~mcnudust/" target="_blank">Dr. McNulty</a>
+      </div>
+      <div style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginLeft: "325px",
+      }}>
+        <Image src="/images/isu_logo.png" alt="Logo" width={40} height={40} style={{ marginRight: "100px" }} />
+
+        <Image src="/images/doe_logo.png" alt="Logo" width={80} height={80} style={{ marginRight: "100px" }} />
+
+        <Image src="/images/jlab_logo.png" alt="Logo" width={160} height={160} style={{ marginRight: "100px" }} />
+      </div>
+    </div>,
   },
+
+  
 
   // Add the useNextSeoProps hook here
   useNextSeoProps() {
